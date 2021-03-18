ccdeb8f994abbaff68e225f61acab307e4e29243
Eduardo David Jr <edjdavid@users.noreply.github.com>
2021-03-18 17:22:29 +0800

    Add answer to question 1

---
+++ answers.py
@@ -0,0 +1,19 @@
+def max_cont_sum(arr):
+    # store the highest sum and compare it with each additional item
+    high_sum = 0
+    curr_sum = 0
+
+    for i in range(len(arr)):
+        curr_sum = curr_sum + arr[i]
+        # reset if we go below zero
+        if curr_sum < 0:
+            curr_sum = 0
+
+        # update the sum if the current sum is higher
+        elif high_sum < curr_sum:
+            high_sum = curr_sum
+    return high_sum
+
+assert max_cont_sum([1, 2, -3, 5]) == 5
+assert max_cont_sum([1, 2, 3, -4, 10]) == 12
+assert max_cont_sum([1, 2, -3, -5, 1]) == 3
